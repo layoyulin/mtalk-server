afc177a713b32804875df63ee02d1ca0edaa3cb7
lay <yl200808@live.cn>
2012-12-20 00:19:39 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VvtalkServer::Application.config.secret_token = '16cdbe271fb6c36798d6dc8b52e4812c63edce31db8deeb6608179acd83a4fa29faaee32cb8bb8b9de9ba2219cb0c7aaaac139a06bb4ee7f2dea647806e4ab28'
